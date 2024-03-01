d701a1b2816af5f0bfddd976116875da68c10b87
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2024-03-01 12:38:03 +0000

    Updated teamproject.sql to include contact table

--- resources/teamproject.sql
+++ resources/teamproject.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 29, 2024 at 12:14 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Mar 01, 2024 at 01:36 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,12 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `ticketID` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `userID` int(8) NOT NULL,
   `username` varchar(20) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
   `userEmail` varchar(254) NOT NULL,
   `userPass` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -38,13 +52,19 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `username`, `userEmail`, `userPass`) VALUES
-(3, 'JDennis21', 'joshua.dennis41@gmail.com', 'e13282409d28e24873c52c7edd5b81d0'),
-(4, 'username', 'user@gmail.com', 'e13282409d28e24873c52c7edd5b81d0');
+INSERT INTO `users` (`userID`, `username`, `fullname`, `userEmail`, `userPass`) VALUES
+(3, 'JDennis21', '', 'joshua.dennis41@gmail.com', 'e13282409d28e24873c52c7edd5b81d0'),
+(4, 'username', '', 'user@gmail.com', 'e13282409d28e24873c52c7edd5b81d0');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`ticketID`);
 
 --
 -- Indexes for table `users`
@@ -55,6 +75,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `ticketID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
