d115171ba33e88725834ebaeec8d6ec6555e9744
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2024-03-10 01:28:58 +0000

    More changes

--- pages/home/index.php
+++ pages/home/index.php
@@ -7,7 +7,7 @@ global $connection;
 <head>
     <meta charset="UTF-8">
     <link href="/css/main.css" rel="stylesheet" type="text/css" />
-    <link href="/css/home.css" rel="stylesheet" type="text/css" />
+    <link href="/css/index.css" rel="stylesheet" type="text/css" />
     <title>Site Name</title>
 </head>
 <body>
