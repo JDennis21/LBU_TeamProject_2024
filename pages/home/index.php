394466eb80829970004c9250132cc0b86b37d4dc
NotoriousMaka <114700306+NotoriousMaka@users.noreply.github.com>
2024-03-10 01:53:31 +0000

    Update index.php

--- pages/home/index.php
+++ pages/home/index.php
@@ -7,14 +7,19 @@ global $connection;
 <head>
     <meta charset="UTF-8">
     <link href="/css/main.css" rel="stylesheet" type="text/css" />
-    <link href="/css/index.css" rel="stylesheet" type="text/css" />
+    <link href="/css/home.css" rel="stylesheet" type="text/css" />
     <title>Site Name</title>
 </head>
 <body>
 <?php include '../components/nav.php'; ?>
 <div class="main">
     <div class="content">
-
+        <p class="text">COMFORT TAILORED TO YOUR HOME, CLIMATE CONTROL MADE SIMPLE.</p>
+    </div>
+    <div class="rightBannerContainer">
+        <div class="rightBanner">
+            <img src="images/img.png" alt="Banner Image">
+        </div>
     </div>
 </div>
 <?php include '../components/footer.html'; ?>
