241466209456d812779b75d540e1cd8d123a1bb9
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2024-03-11 19:58:06 +0000

    Changes for contact page sizing and font

--- pages/contact/contact.php
+++ pages/contact/contact.php
@@ -14,41 +14,46 @@ global $connection;
 <?php include '../components/nav.php'; ?>
 <div class="main">
     <div class="content">
-        <?php
-        /*
-         * If the session error isset() set the variable to "noMargin" else the variable equals ""
-         *
-         * This is used to remove the margin on form inputs when an error is printed to help with
-         * formatting
-        */
-        $inputClass = isset($_SESSION["error"]) ? 'noMargin' : '';
-        ?>
-        <div class="contactForm">
-            <form action="../auth/login/contact.php" method="post">
-                <div class="formContent">
-                    <h2 style="font-weight: bolder;">GET IN TOUCH</h2>
+        <div class="formContainer">
+            <div class="contactForm">
+                <form>
+                    <h2>GET IN TOUCH</h2>
                     <div class="inputRow">
-                        <input placeholder="NAME" type="text" name="name" id="contactName" value="<?php echo $_SESSION['nameAttempt'] ?? '' ?>">
-                        <input placeholder="EMAIL" type="email" name="email" id="contactEmail" value="<?php echo $_SESSION['emailAttempt'] ?? '' ?>">
+                        <!--suppress HtmlFormInputWithoutLabel -->
+                        <input placeholder="NAME" type="text" name="name" id="contactName"
+                               value="<?php echo $_SESSION['nameAttempt'] ?? '' ?>">
+                        <!--suppress HtmlFormInputWithoutLabel -->
+                        <input placeholder="EMAIL" type="email" name="email" id="contactEmail"
+                               value="<?php echo $_SESSION['emailAttempt'] ?? '' ?>">
                     </div>
-                    <textarea placeholder="MESSAGE" name="message" id="contactMessage" class="<?php echo $inputClass; ?>"></textarea>
-                    <?php
-                    if (isset($_SESSION["error"])) {
-                        echo '<p class="error">*' . $_SESSION["error"] . '</p>';
-                    }
-                    ?>
-                    <div>
-                        <input type="submit" value="SUBMIT">
+                    <!--suppress HtmlFormInputWithoutLabel -->
+                    <textarea placeholder="MESSAGE" name="message" id="contactMessage"></textarea>
+                    <input type="submit" value="SUBMIT">
+                </form>
+            </div>
+            <div class="contactBanner">
+                <div class="bannerHeader">
+                    <h1>Contact Us</h1>
+                </div>
+                <div class="bannerContent">
+                    <div class="bannerInfo">
+                        <div class="circle"></div>
+                        <h3>Address</h3>
+                    </div>
+                    <div class="bannerInfo">
+                        <div class="circle"></div>
+                        <h3>Phone</h3>
+                    </div>
+                    <div class="bannerInfo">
+                        <div class="circle"></div>
+                        <h3>Email</h3>
+                    </div>
+                    <div class="bannerInfo">
+                        <div class="circle"></div>
+                        <h3>Website</h3>
                     </div>
                 </div>
-            </form>
-        </div>
-        <div class="contactBanner">
-            <h1>Contact Us</h1>
-            <h3 id="address">Address</h3>
-            <h3>Phone</h3>
-            <h3>Email</h3>
-            <h3>Website</h3>
+            </div>
         </div>
     </div>
 </div>
